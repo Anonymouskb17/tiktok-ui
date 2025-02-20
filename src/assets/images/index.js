40aa2573268872dde48349b00d866c7716bc86ab
Anonymouskb17 <mancity452k3@gmail.com>
2025-02-21 01:54:50 +0700

    fix header ui

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,6 +1,6 @@
-const imgaes = {
+const image = {
   logo: require("~/assets/images/logo.svg").default,
-  noimage: require("~/assets/images/no-image.png").default,
+  noimage: require("~/assets/images/no-image.png"),
 };
 
-export default imgaes;
+export default image;
--- src/components/Layout/component/Header/index.js
+++ src/components/Layout/component/Header/index.js
@@ -134,7 +134,7 @@ function Header() {
                 src="https://lh7-rt.googleusercontent.com/docsz/AD_4nXdjpkHE8IpiRbkacFfKEgXL12_aXj8DbbCCHwd5RbiqDb-vHRIeqNlIhkbttVTDyDc7d_ilwwRUVyx9FNT3V9PL-RcmdVaZXEI0hhonRhHQWICN3Or7fSL5xGENFLud5r7DfWq49HbkF5ISQOnLWQ=s412?key=rNRmiFjobxteGc5w7mKQIg"
                 className={cx("user-avatar")}
                 alt="Nguyen van a"
-                fallback=""
+                fallback
               />
             ) : (
               <button className={cx("more-btn")}>
